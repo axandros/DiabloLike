17da373744262c78c8b03186b2d865112558cf3d
Brumagint/Desktop <brumagint@gmail.com>
2021-03-21 16:51:52 -0400

    Implemented getting the tile to spawn.

--- Source/DiabloLike/Private/DungeonSpawner.cpp
+++ Source/DiabloLike/Private/DungeonSpawner.cpp
@@ -52,45 +52,65 @@ void UDungeonSpawner::SpawnTile(FGenericTile tile, int gridX, int gridY)
 	const int SOUTH = FGenericTile::fSOUTH;
 	const int WEST = FGenericTile::fWEST;
 
+	TSoftObjectPtr<UWorld> worldToSpawn;
+
 	switch (flags) {
 		case 0 : // none
+			worldToSpawn = RandomTile(Tileset->NoExit_Tiles);
 			break;
 		case NORTH : 
+			worldToSpawn = RandomTile(Tileset->N_Tiles);
 			break;
 		case EAST: // East only
+			worldToSpawn = RandomTile(Tileset->E_Tiles);
 			break;
 		case SOUTH: // South only
+			worldToSpawn = RandomTile(Tileset->S_Tiles);
 			break;
 		case WEST: // West only
+			worldToSpawn = RandomTile(Tileset->W_Tiles);
 			break;
 		case (NORTH|EAST):
+			worldToSpawn = RandomTile(Tileset->NE_Tiles);
 			break;
 		case (NORTH|SOUTH):
+			worldToSpawn = RandomTile(Tileset->NS_Tiles);
 			break;
 		case (NORTH|WEST):
+			worldToSpawn = RandomTile(Tileset->NW_Tiles);
 			break;
 		case (EAST|SOUTH):
+			worldToSpawn = RandomTile(Tileset->ES_Tiles);
 			break;
 		case (EAST|WEST):
+			worldToSpawn = RandomTile(Tileset->EW_Tiles);
 			break;
 		case (SOUTH|WEST):
+			worldToSpawn = RandomTile(Tileset->SW_Tiles);
 			break;
 		case (NORTH|EAST|SOUTH):
+			worldToSpawn = RandomTile(Tileset->NES_Tiles);
 			break;
 		case (NORTH|EAST|WEST):
+			worldToSpawn = RandomTile(Tileset->NEW_Tiles);
 			break;
 		case (NORTH|SOUTH|WEST):
+			worldToSpawn = RandomTile(Tileset->NSW_Tiles);
 			break;
 		case (EAST|SOUTH|WEST):
+			worldToSpawn = RandomTile(Tileset->ESW_Tiles);
 			break;
 		case (NORTH|EAST | SOUTH | WEST):
+			worldToSpawn = RandomTile(Tileset->NESW_Tiles);
 			break;
 	}
 }
 
 TSoftObjectPtr<UWorld> UDungeonSpawner::RandomTile(TArray<TSoftObjectPtr<UWorld>> tilesetCollection)
 {
-	return TSoftObjectPtr<UWorld>();
+	int size = tilesetCollection.Num();
+	int randNum = FMath::RandRange(0, size);
+	return tilesetCollection[randNum];
 }
 
 float UDungeonSpawner::percentLoadedLevels()
